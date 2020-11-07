7af67023697ebcf44a91d8c753caea5e2a90a698
Baptiste ROSSIGNOL <baptiste.rossignol@hotmail.fr>
2020-11-07 16:24:52 +0100

    ADD: Exercice 2

--- exercise-2/string.js
+++ exercise-2/string.js
@@ -44,11 +44,62 @@ function type_check_v2(value, conf) {
     return true;
 }
 
-// Type_check_v1
-console.log(type_check_v1(1, "number"));
 
-// Type_check_v2
-console.log(type_check_v2({prop1:1}, {type:"object"}));
-console.log(type_check_v2("foo", {type: "string", value: "foo"}));
-console.log(type_check_v2("bar", {type: "string", value: "foo"}));
-console.log(type_check_v2(3, {enum: ["foo", "bar", 3]}));
+function type_check(data, config) {
+    if (type_check_v1(data, config.type)) {
+        if (config.hasOwnProperty("properties")) {
+            for (key in config.properties) {
+                if (config.properties[key].hasOwnProperty("properties") && !type_check(data[key], config.properties[key]) || !type_check_v2(data[key], config.properties[key])) {
+                    return false; 
+                }
+            }
+        } else {
+            if (!type_check_v2(data, config)) return false; 
+        }
+
+        return true;
+    }
+
+    return false;
+}
+
+
+// Jeu de tests pour type_check
+console.log(type_check(1, { type: "number", value: 1 }) === true);
+console.log(type_check(1, { type: "number", value: 3 }) === false);
+console.log(type_check(1, { type: "object", value: 1 }) === false);
+console.log(
+  type_check("string", { type: "string", enum: ["string1", "string2"] }) ===
+    false
+);
+console.log(
+  type_check({ bar: "foo" }, { type: "object", value: { bar: "foo" } }) === true
+);
+console.log(
+  type_check({ bar: "foo" }, { type: "object", value: { bar: "value" } }) ===
+    false
+);
+console.log(
+  type_check(
+    {
+      toto: {
+        fi: 3,
+        fa: {
+          trim: " test ",
+        },
+      },
+    },
+    {
+      type: "object",
+      properties: {
+        toto: {
+          type: "object",
+          properties: {
+            fi: { value: 3 },
+            fa: { enum: [3, "string", { trim: " test " }] },
+          },
+        },
+      },
+    }
+  ) === true
+);
